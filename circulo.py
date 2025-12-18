e4ac8bb8a0b71537fdbdfd317d142446cc340fef
diana704 <francodiana704@gmail.com>
2025-12-18 10:24:37 -0600

    cree el codigo circulo.py alexa

---
+++ circulo.py
@@ -0,0 +1,8 @@
+import math
+
+# Calcular el perímetro de un círculo
+
+radio = float(input("Ingresa el radio del círculo: "))
+perimetro = 2 * math.pi * radio
+
+print("El perímetro del círculo es:", perimetro)
