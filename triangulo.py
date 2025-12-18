85f447e44ce936b481d26eba35bafb2aba09128a
aixa-source <aixa8868@gmail.com>
2025-12-18 10:40:49 -0600

    cree el codigo de triangulo .py angel

---
+++ triangulo.py
@@ -0,0 +1,8 @@
+def triangulo():
+  lada1=float(input("ingresa el lado 1 del triangulo"))
+  lado2=float(input("ingresa el lado 2 del  triangulo"))
+  lado3=float(input("ingresa el lado 3 del triangulo"))
+
+  perimetro=(lada1+lado2+lado3)
+  print("EL PERIMETRO DEL TRIANGULO ES: ",perimetro)
+triangulo()
